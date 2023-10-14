29dbcb86ff3f5cfe207195cb5831c48310be9e6c
LennartWinter <D296433@edu.curio.nl>
2023-10-14 20:37:03 +0200

    Added caesar's cipher

---
+++ encryption/Caesar_cipher.py
@@ -0,0 +1,81 @@
+defaultAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def encrypt(text, shift, alphabet = defaultAlphabet):
+    alphabet = [*alphabet]
+    alphabetLength = len(alphabet)
+    shiftedAlphabet = []
+
+    for char in alphabet:
+        index = alphabet.index(char) + shift
+        if index >= alphabetLength:
+            index = index - alphabetLength
+        shiftedAlphabet.append(alphabet[index])
+
+    textList = [*text]
+    encryptedText = ""
+    for letter in textList:
+        found = False
+        for char in alphabet:
+            if letter.upper() == char:
+                found = True
+                index = alphabet.index(char)
+                encryptedText += shiftedAlphabet[index]
+                break
+        if not found:
+            encryptedText += letter
+
+    return encryptedText
+
+def decrypt(text, shift, alphabet = defaultAlphabet):
+    alphabet = [*alphabet]
+    alphabetLength = len(alphabet)
+    shiftedAlphabet = []
+
+    for char in alphabet:
+        index = alphabet.index(char) - shift
+        if index >= alphabetLength:
+            index = index - alphabetLength
+        shiftedAlphabet.append(alphabet[index])
+
+    textList = [*text]
+    decryptedText = ""
+    for letter in textList:
+        found = False
+        for char in alphabet:
+            if letter.upper() == char:
+                found = True
+                index = alphabet.index(char)
+                decryptedText += shiftedAlphabet[index]
+                break
+        if not found:
+            decryptedText += letter
+
+    return decryptedText
+
+def bruteforce(text, alphabet = defaultAlphabet):
+    alphabet = [*alphabet]
+    alphabetLength = len(alphabet)
+    outputList = []
+    for shift in range(alphabetLength):
+        shiftedAlphabet = []
+
+        for char in alphabet:
+            index = alphabet.index(char) - shift
+            if index >= alphabetLength:
+                index = index - alphabetLength
+            shiftedAlphabet.append(alphabet[index])
+
+        textList = [*text]
+        decryptedText = ""
+        for letter in textList:
+            found = False
+            for char in alphabet:
+                if letter.upper() == char:
+                    found = True
+                    index = alphabet.index(char)
+                    decryptedText += shiftedAlphabet[index]
+                    break
+            if not found:
+                decryptedText += letter
+        outputList.append(f"{shift}. {decryptedText}")
+    return outputList
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import encryption.A1Z26 as A1Z26
+import encryption.Caesar_cipher as Caesar
 
-text = "Helloworld"
-encryptedText = A1Z26.encrypt(text)
-print(encryptedText)
-decryptedText = A1Z26.decrypt(encryptedText)
-print(decryptedText)
+a = Caesar.encrypt("This is fun!", 5)
+print(Caesar.bruteforce(a))
